5f2200af9fcc7768db4cded3f05087252d62d3ed
Philip Darke <43066442+philipdarke@users.noreply.github.com>
2021-11-05 16:57:36 +0000

    Improve memory management

--- run_all.R
+++ run_all.R
@@ -1,21 +1,35 @@
 # Run all scripts
 # Author: Philip Darke <p.a.darke2@newcastle.ac.uk>
 
+#' Clear environment before sourcing script
+#'
+#' @param path Path to .R script
+#'
+#' @return NULL
+#' 
+#' @export
+#'
+source_gc <- function (path) {
+  rm(list = ls())
+  gc()
+  source(path)
+}
+
 # 1. Infer periods of EHR data collection across data providers
-source("01_prepare_data/01_subset_visit_data.R")
-source("01_prepare_data/02_clean_data.R")
-source("01_prepare_data/03_reg_history.R")
-source("01_prepare_data/04_data_period.R")
+source_gc("01_prepare_data/01_subset_visit_data.R")
+source_gc("01_prepare_data/02_clean_data.R")
+source_gc("01_prepare_data/03_reg_history.R")
+source_gc("01_prepare_data/04_data_period.R")
 
 # 2. Extract biomarkers etc from EHR and UK Biobank visit data
-source("02_extract_records/01_filter_data.R")
-source("02_extract_records/02_anthropomorphic.R")
-source("02_extract_records/03_blood_tests.R")
-source("02_extract_records/04_demographic.R")
-source("02_extract_records/05_family_history.R")
-source("02_extract_records/06_diagnoses.R")
-source("02_extract_records/07_prescriptions.R")
+source_gc("02_extract_records/01_filter_data.R")
+source_gc("02_extract_records/02_anthropomorphic.R")
+source_gc("02_extract_records/03_blood_tests.R")
+source_gc("02_extract_records/04_demographic.R")
+source_gc("02_extract_records/05_family_history.R")
+source_gc("02_extract_records/06_diagnoses.R")
+source_gc("02_extract_records/07_prescriptions.R")
 
 # 3. Diabetes phenotyping case study
-source("01_prepare_data/05_secondary_care.R")
-source("02_diabetes_phenotyping.R")
+source_gc("01_prepare_data/05_secondary_care.R")
+source_gc("02_diabetes_phenotyping.R")
